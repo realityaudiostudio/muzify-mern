41fbc13b783d1613465ca26461a22d6cbda9c669
Alan Jose Santo <realityaudiostudio@gmail.com>
2024-07-12 23:36:28 +0530

    HEADER CMPLTE

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import "./header.css";
 import Piano from '../../images/pno.png';
+import { ShoppingCart,DotsThree } from "@phosphor-icons/react";
+
 
 function Header() {
   return (
     <div className='mainHead'>
         <div className="dataas">
-            <p>CLASSIC PIANO 213</p>
-            <p></p>
+            <p className='lth'>CLASSIC PIANO 213</p>
+            <p className='bigh'>Meet the pure<br></br>PIANO Sound !</p>
+            <p className='lth'>Discover the enchanting world of<br></br>pianos with our exquisite collection.</p>
+            <div className="bybtn">
+                <button className='byn'>Buy Now <ShoppingCart size={24} /></button>
+                <button className='sem'>See More <DotsThree size={24} /></button>
+            </div>
         </div>
         <div className="pno">
             <img src={Piano} alt='Piano'></img>
